cbf78caba55370fb4981816da8acea510e866e3e
michalzmujdzin <michal.zmujdzin@movecloser.pl>
2017-10-22 23:10:07 +0200

    some initial stuff to tryout node

---
+++ app.js
@@ -0,0 +1,5 @@
+const stuff = require('./stuff');
+
+console.log(stuff.counter(['1', '2']));
+console.log(stuff.adder(1,2));
+console.log(stuff.pi);
---
+++ stuff.js
@@ -0,0 +1,5 @@
+module.exports.counter = (arr) => 'There are ' + arr.length + ' elements in this array';
+
+module.exports.adder = (a, b) => `The sum of the two numbers is ${a + b}`;
+
+module.exports.pi = 3.142;
